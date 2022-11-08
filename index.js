4ead63fe93ad1403f276bfb94638ba0d1d1a0207
saadhossain <mshsarkar@gmail.com>
2022-11-08 11:45:04 +0600

    MongoDB Connected and Configured  and Services api created

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const { MongoClient } = require('mongodb')
+//Middle Wares
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+//setup MongoDB
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
+//DataBase Connection and Apis
+const dbConnect = () => {
+    const services = client.db("laura-jane").collection("services");
+
+    //Get Services from MongoDB
+    app.get('/services', async(req, res)=> {
+        const query = {};
+        const cursor = services.find(query)
+        const result = await cursor.limit(3).toArray();
+        res.send(result)
+    })
+
+}
+dbConnect()
+
+//Server Default/root route
+app.get('/', (req, res)=> {
+    res.send('Laura Jane Server is Running...')
+})
+
+//Add a listener
+app.listen(port, ()=> {
+    console.log('Server Running On port: ',port);
+})
